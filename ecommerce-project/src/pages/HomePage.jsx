cbec679881989c137f6155b79d7abfa49fed8549
Samuel <samueljuliank29@gmail.com>
2025-08-24 14:35:23 +0800

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 const HomePage = () => {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+          console.log(data)
+      })
+
   return (
     <>
       <title>Ecommerce Project</title>
